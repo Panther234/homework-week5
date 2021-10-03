574f939c83a11cd2e4702dcb762baf3eb5e81a44
Panther234 <91629218+Panther234@users.noreply.github.com>
2021-10-03 12:33:35 +0100

    Add files via upload

---
+++ Face.java
@@ -0,0 +1,13 @@
+public class Face {
+    /*
+    Line 1
+    Line 2
+    */
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| 0 0 |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '_' | ");
+        System.out.println(" +_____+ ");
+    }
+}
